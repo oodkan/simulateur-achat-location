79d54968664d64d7d2bad1fac78477759b529f7f
oodkan <8947118+oodkan@users.noreply.github.com>
2023-08-08 21:43:17 +0200

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '//simulateur-achat-location/',
+  base: '/simulateur-achat-location/',
 });
